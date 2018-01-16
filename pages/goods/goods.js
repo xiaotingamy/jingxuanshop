3dc07bf427f3c7a32d46849a7f6e46e06a51e976
xiaotingamy <502150736@qq.com>
2018-01-16 17:17:33 +0800

    first commit

---
+++ pages/goods/goods.js
@@ -0,0 +1,73 @@
+// pages/goods/goods.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    gallery: [{
+      id: 1,
+      img_url: 'http://yanxuan.nosdn.127.net/960e2718a8a4ebdf049ab42280253aa3.jpg?imageView&thumbnail=750x0&quality=75'
+    },{
+      id: 2,
+      img_url: 'http://yanxuan.nosdn.127.net/ff485de9150a1d1f6e9004f15c853972.jpg?imageView&thumbnail=750x0&quality=75'
+    }],
+    openAttr: true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
